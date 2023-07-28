d2c4363b2bb99b80abfc5c0f7a1ea3fab29a9947
quang <nhutquangnguyen11192@gmail.com>
2023-07-28 23:55:08 +0900

    update code

--- src/redux/store.config.js
+++ src/redux/store.config.js
@@ -1,11 +1,15 @@
-import {createStore, combineReducers} from "redux";
+import { createStore, combineReducers } from "redux";
+import DatVeReducer from "./dat-ve/dat-ve.reducer";
 
-const rootReducer = combineReducer ({});
 
-// Redux store, nơi tập trung các state của ứng dụng
+const rootReducer = combineReducers({
+  DatVeReducer,
+});
 
-export const store = createStore (
-    rootReducer, /* preloadedState, */
-    // extension cho redux.
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// redux store. tập trung tất cả state của ứng dụng.
+
+export const store = createStore(
+  rootReducer,
+  // extension cho redux.
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
